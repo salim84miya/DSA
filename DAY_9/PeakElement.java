6cac169d7e8ba899e2caa081f020cdefa38131f2
salim84miya <salimsaudagar84@gmail.com>
2024-06-26 22:35:06 +0530

    solved peak element problem

--- DAY_9/PeakElement.java
+++ DAY_9/PeakElement.java
@@ -7,18 +7,30 @@ public class PeakElement {
     }
     
     static int find(int[] arr){
-        int start = 0;
-        int end = arr.length-1;
+
+      if(arr.length==1){
+        return 0;
+      }
+
+      if(arr[0]>arr[1]){
+        return 0;
+      }
+
+      if(arr[arr.length-1]>arr[arr.length-2]){
+        return arr.length-1;
+      }
+        int start = 1;
+        int end = arr.length-2;
         while(start<=end){
           int mid = start+(end-start)/2;
-          if(mid<end && arr[mid]>arr[mid+1] && mid>start && arr[mid]>arr[mid-1]){
-              return arr[mid];
-          }
-          if (mid<end && arr[mid]<arr[mid+1]){
-            start = mid;
-          }
-          if(mid>start && arr[mid]<arr[mid-1]){
-            end=mid;
+          if(arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1]){
+              return mid;
+          }else if (arr[mid]<arr[mid+1]){
+            start = mid+1;
+          }else if( arr[mid]<arr[mid-1]){
+            end=mid-1;
+          }else{
+            start=mid+1;
           }
         }
         return -1;
